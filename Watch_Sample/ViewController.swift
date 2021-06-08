ecab28af1927fe3c3e14fab73846387fb942ac2d
en-i <templebond@icloud.com>
2021-06-09 01:44:37 +0900

    Initial Commit

---
+++ Watch_Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Watch_Sample
+//
+//  Created by terada enishi on 2021/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
